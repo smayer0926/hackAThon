b3300e511a5c0551edd342f5c20945fec467f13c
Stephanie Mayer <smayer0926@gmail.com>
2017-08-11 09:36:51 -0700

    add before/after tests to clear array

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -1,10 +1,19 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TeamTest {
+    @After
+    public void tearDown () throws Exception {
+        Team.clearAllTeams();
+    }
+    @Before
+    public void setUp() throws Exception {
+    }
     @Test
     public void Team_instantiatesCorrectly_true() throws Exception {
         Team testTeam = new Team("PDX");
@@ -18,7 +27,6 @@ public class TeamTest {
 
     @Test
     public void all_returnsAllTeams_2() throws Exception {
-        Team.clear();
         Team one = new Team("PDX");
         Team two = new Team("Ducks");
         assertEquals(2, Team.getAllTeams().size());
@@ -26,13 +34,19 @@ public class TeamTest {
 
     @Test
     public void getTeamId_teamInstantiatesWithanId() throws Exception {
-        Team.clear();
         Team testTeam = new Team ("PDX");
         assertEquals(1, testTeam.getTeamId());
     }
     public void findReturnsCorrectName() throws Exception {
         Team testTeam = newTeam();
         assertEquals(1, Team.findById(testTeam.getTeamId()).getTeamId());
+    }
+    @Test
+    public void findReturnsCorrectTeamWhenMoreThanOneNameExists() throws Exception {
+
+        Team testTeam = newTeam();
+        Team otherTeam = new Team("Ducks");
+        assertEquals(2, Team.findById(otherTeam.getTeamId()).getTeamId());
     }
 
     //Helpers
