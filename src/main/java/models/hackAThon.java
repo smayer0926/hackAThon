83dabb55cc96f8c317ebd9265ffdf3a9ddb2a704
Stephanie Mayer <smayer0926@gmail.com>
2017-08-11 08:19:26 -0700

    create method for third test

--- src/main/java/models/hackAThon.java
+++ src/main/java/models/hackAThon.java
@@ -1,17 +1,27 @@
 package models;
 
 
+import java.util.ArrayList;
+
 public class hackAThon {
 
 
     private String name;
 
+
+    private static ArrayList<hackAThon> allEvents = new ArrayList<hackAThon>();
+
     public hackAThon(String name) {
         this.name = name;
+        allEvents.add(this);
     }
 
 //Getters
     public String getName() {
         return name;
     }
+    public static ArrayList<hackAThon> getAllEvents() {
+        return allEvents;
+    }
+
 }
--- src/test/java/models/hackAThonTest.java
+++ src/test/java/models/hackAThonTest.java
@@ -1,9 +1,12 @@
 package models;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
 import org.junit.Test;
 
 public class hackAThonTest {
+
     @Test
     public void hackAThon_instantiatesCorrectly() throws Exception{
         hackAThon event = new hackAThon("Jim");
@@ -11,8 +14,14 @@ public class hackAThonTest {
     }
 
     @Test
-    public void hackAThon_entryInstantiatesWithConent_Name() throws Exception {
+    public void hackAThon_entryInstantiatesWithContent_Jim() throws Exception {
         hackAThon event = new hackAThon("Jim");
         assertEquals("Jim", event.getName());
+    }
+
+    @Test
+    public void hackAThon_entryReturnsContent_1() throws Exception {
+        hackAThon event = new hackAThon("Jim");
+        assertEquals(1, event.getAllEvents().size());
     }
 }
