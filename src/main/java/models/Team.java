e87136f9c7ef003c2888d877a706b64695e2e60c
Stephanie Mayer <smayer0926@gmail.com>
2017-08-11 09:15:20 -0700

    add management page for a team

---
+++ src/main/java/models/Team.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Team {
+    private String nameOfTeam;
+    private static ArrayList<Team> allTeams = new ArrayList<Team>();
+    private int teamId;
+    private ArrayList<Member> teamMembers;
+
+    public Team(String nameOfTeam) {
+        this.nameOfTeam = nameOfTeam;
+        allTeams.add(this);
+        teamId = allTeams.size();
+        teamMembers = new ArrayList<Member>();
+    }
+    public static void clear() {
+        allTeams.clear();
+    }
+    public static Team findById(int id){
+        return allTeams.get(id-1);
+    }
+    public void addMember(Member member){
+        teamMembers.add(member);
+    }
+
+    //Getters
+    public String getNameOfTeam() {
+        return nameOfTeam;
+    }
+
+    public static ArrayList<Team> getAllTeams() {
+        return allTeams;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public ArrayList<Member> getTeamMembers() {
+        return teamMembers;
+    }
+}
+
