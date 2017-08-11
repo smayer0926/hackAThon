3f8aa66f39162085f56c077c3777f10163f12a39
Stephanie Mayer <smayer0926@gmail.com>
2017-08-11 08:21:02 -0700

    create after/before tests

--- src/test/java/models/hackAThonTest.java
+++ src/test/java/models/hackAThonTest.java
@@ -3,10 +3,17 @@ package models;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class hackAThonTest {
-
+    @After
+    public void tearDown () throws Exception {
+        hackAThon.clearAllEvents();
+    }
+    @Before
+    public void setUp() throws Exception {
+    }
     @Test
     public void hackAThon_instantiatesCorrectly() throws Exception{
         hackAThon event = new hackAThon("Jim");
