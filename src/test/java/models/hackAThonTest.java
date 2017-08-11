3c4fbf99678cf7c1170b466d5220c17635a8e1a3
Stephanie Mayer <smayer0926@gmail.com>
2017-08-11 08:41:31 -0700

    add test to find/return correct name, add helper

--- src/test/java/models/hackAThonTest.java
+++ src/test/java/models/hackAThonTest.java
@@ -19,23 +19,30 @@ public class hackAThonTest {
         hackAThon event = new hackAThon("Jim");
         assertEquals(true, event instanceof hackAThon);
     }
-
     @Test
     public void hackAThon_entryInstantiatesWithContent_Jim() throws Exception {
         hackAThon event = new hackAThon("Jim");
         assertEquals("Jim", event.getName());
     }
-
     @Test
     public void hackAThon_entryReturnsContent_1() throws Exception {
         hackAThon event = new hackAThon("Jim");
         assertEquals(1, event.getAllEvents().size());
     }
-
     @Test
     public void getId_eventInstantiatesWithAnID_1() throws Exception {
         hackAThon.clearAllEvents();
         hackAThon myEvent = new hackAThon("Jim");
         assertEquals(1, myEvent.getId());
+    }
+
+    @Test
+    public void findReturnsCorrectName() throws Exception {
+        hackAThon event = newEvents();
+        assertEquals(1, hackAThon.findById(event.getId()).getId());
+    }
+    //Helpers
+    public hackAThon newEvents() {
+        return new hackAThon("Jim");
     }
 }
