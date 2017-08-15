3a490a9860119fcb3b95015105eff5f37deda0b7
smayer0926 <smayer0926@gmail.com>
2017-08-14 20:25:28 -0700

    Update TeamTest.java

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -7,45 +7,54 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TeamTest {
-    @After
-    public void tearDown () throws Exception {
-        Team.clearAllTeams();
-    }
-    @Before
+     @Before
     public void setUp() throws Exception {
+    }
+    @After
+    public void tearDown() throws Exception {
+        Team.clearAllTeams();
     }
     @Test
     public void Team_instantiatesCorrectly_true() throws Exception {
-        Team testTeam = new Team("PDX");
+        Team testTeam = new Team("PDX", "name");
         assertEquals(true, testTeam instanceof Team);
     }
     @Test
     public void getTeamName_teamInstantiatesWithName_PDX() throws Exception {
-        Team testTeam = new Team("PDX");
+        Team testTeam = new Team("PDX", "name");
         assertEquals("PDX", testTeam.getNameOfTeam());
     }
-
     @Test
     public void all_returnsAllTeams_2() throws Exception {
-        Team one = new Team("PDX");
-        Team two = new Team("Ducks");
+        Team one = new Team("PDX", "name");
+        Team two = new Team("Ducks", "name");
         assertEquals(2, Team.getAllTeams().size());
     }
-
     @Test
     public void getTeamId_teamInstantiatesWithanId() throws Exception {
-        Team testTeam = new Team ("PDX");
-        assertEquals(1, testTeam.getId());
+        Team testTeam = new Team ("PDX", "name");
+        assertEquals(3, testTeam.getId());
     }
     public void findReturnsCorrectName() throws Exception {
         Team testTeam = newTeam();
-        assertEquals(1, Team.findById(testTeam.getId()).getId());
+        assertEquals(1, Team.findbyId(testTeam.getId()).getId());
     }
+
     @Test
-    public void findReturnsCorrectTeamWhenMoreThanOneNameExists() throws Exception {
+    public void clearMembersfromTeam_0() throws Exception {
+         Team testTeam = newTeam();
+         Team newTeam = new Team("Seattle", "new");
+         testTeam.setMembers("New", "new");
+         Team.clearAllTeams();
+         assertEquals(0, Team.getAllTeams().size());
+    }
+
+    @Test
+    public void Team_CreatesUniqueID_() throws Exception {
+        Team.clearAllTeams();
         Team testTeam = newTeam();
-        Team otherTeam = new Team("Ducks");
-        assertEquals(2, Team.findById(otherTeam.getId()).getId());
+        Team newTeam = new Team("Seattle", "new");
+        assertEquals(7, newTeam.getId());
     }
     @Test
     public void updateChangestoTeamContent() throws Exception {
@@ -58,9 +67,8 @@ public class TeamTest {
         assertEquals(formerId, testTeam.getId());
         assertNotEquals(formerContent, testTeam.getNameOfTeam());
     }
-
     //Helpers
     public Team newTeam() {
-        return new Team("PDX");
+        return new Team("PDX", "name");
     }
 }
